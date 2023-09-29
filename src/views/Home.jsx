8f66ea36336a1307c6a197e8ad535ef3e023a28d
Aiman Aizuddin <aimanaizuddin30@gmail.com>
2023-09-29 11:05:06 +0800

    Fetch task from firestore database

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,19 +1,41 @@
+import { useState, useEffect } from 'react';
 import TaskForm from '../components/TaskForm';
 import TaskList from '../components/TaskList';
 import 'tailwindcss/tailwind.css';
+import { db } from '../../firebaseConfig';
+import { collection, getDocs } from "@firebase/firestore";
 
 const Home = () => {
-  const tasks = []; // Placeholder
+    const [tasks, setTasks] = useState([]);
 
-  return (
-    <div className="min-h-screen bg-black-100 flex flex-col items-center justify-center">
-      <div className="bg-black p-8 rounded-lg shadow-md w-96">
-        <h1 className="text-2xl font-bold mb-4 text-white">Task Manager</h1> {/* Added text-white class */}
-        <TaskForm />
-        <TaskList tasks={tasks} />
-      </div>
-    </div>
-  );
+    useEffect(() => {
+        const fetchTasks = async () => {
+            try {
+                const tasksCollection = collection(db, 'tasks');
+                const snapshot = await getDocs(tasksCollection);
+                const fetchedTasks = snapshot.docs.map(doc => ({
+                    id: doc.id,
+                    ...doc.data()
+                }));
+                setTasks(fetchedTasks);
+            } catch (error) {
+                console.error("Error fetching tasks:", error);
+            }
+        };
+        
+    fetchTasks();
+    }, []);
+
+    return (
+        <div className="min-h-screen bg-black-100 flex flex-col items-center justify-center">
+            <div className="bg-black p-8 rounded-lg shadow-md w-150">
+                <h1 className="text-2xl font-bold mb-4 text-white">Task Manager</h1>
+                <TaskForm />
+                <TaskList tasks={tasks} />
+            </div>
+        </div>
+    );
 };
 
 export default Home;
+
